030fa70253df3d08c019a5a12338378d138d57d9
Anshumancoder <anshu.pro.real@gmail.com>
2024-11-07 15:39:53 +0530

    adding js and css for navbar

---
+++ Script.js
@@ -0,0 +1,9 @@
+function filterLinks() {
+    const input = document.getElementById("searchInput").value.toLowerCase();
+    const navLinks = document.querySelectorAll("#navbarLinks .nav-item");
+
+    navLinks.forEach(link => {
+      const linkText = link.textContent.toLowerCase();
+      link.style.display = linkText.includes(input) ? "" : "none";
+    });
+  }
